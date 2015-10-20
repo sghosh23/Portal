c634857e3b2a7ea8b530c898141516dcec7a9661
amisukanta <amisukanta02@gmail.com>
2015-10-20 18:25:10 +0200

    cross domain factor

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module Accounts
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOWALL'
+    }
     config.active_record.raise_in_transactional_callbacks = true
 
     config.react.addons = true
